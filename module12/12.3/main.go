ac3dac5175ee00ddb9553091da16c5ff26ed6160
alef-adm <ireg_2@mail.ru>
2024-02-04 20:38:58 +0300

    12.3

---
+++ module12/12.3/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano()) // необходимо для того, чтобы рандом был похож на рандомный
+}
+
+func main() {
+	ar := make([]int, 5)
+	for i := range ar {
+		ar[i] = rand.Intn(200) - 100 // ограничиваем случайное значение от [-100;100]
+	}
+
+	bubbleSort(ar)
+	bubbleSortReversed(ar)
+	fmt.Println(ar)
+}
+
+func bubbleSort(ar []int) {
+
+	for i := 0; i < len(ar); i++ {
+		c := 0
+		for j := 0; j < len(ar); j++ {
+
+			if j < len(ar)-1 {
+				if ar[j] > ar[j+1] {
+					ar[j], ar[j+1] = ar[j+1], ar[j]
+					c++
+				}
+			}
+			fmt.Println(ar, c)
+
+		}
+		if c == 0 {
+			break
+		}
+	}
+}
+
+func bubbleSortReversed(ar []int) {
+	for i := 0; i < len(ar); i++ {
+		c := 0
+		for j := 0; j < len(ar); j++ {
+
+			if j < len(ar)-1 {
+				if ar[j] < ar[j+1] {
+					ar[j], ar[j+1] = ar[j+1], ar[j]
+					c++
+				}
+			}
+			fmt.Println(ar, c)
+
+		}
+		if c == 0 {
+			break
+		}
+	}
+}
--- tests/main.go
+++ tests/main.go
@@ -1,66 +1,8 @@
 package main
 
+import "fmt"
+
 func main() {
-
+	slice := []int{1, 2, 3, 4, 5}
+	fmt.Println(slice)
 }
-func init()  {
-	rand.Seed(time.Now().Unix())
-}
-
-// RandomMatrix generate square matrix of size n and fills cells with random values
-func RandomMatrix(n int) [][]int {
-	randMatrix := make([][]int, n)
-	for i := range randMatrix {
-		randMatrix[i] = make([]int, n)
-	}
-	rand
-	for i := 0; i < n; i++ {
-		for j := 0; j < n; j++ {
-			randMatrix[i][j] = rand.Int()
-		}
-	}
-
-	return randMatrix
-}
-//type UnitType string
-//
-//type Unit struct {
-//	Value float64
-//	T     UnitType
-//}
-//
-//type Dimensions interface {
-//	Length() Unit
-//}
-//
-//type Auto interface {
-//	Brand() string
-//}
-//type automobile struct {
-//	brand    string
-//	model    string
-//	len      Unit
-//	width    Unit
-//	height   Unit
-//	maxspeed int
-//	engine   int
-//}
-//
-//func (a *automobile) Brand() string {
-//	return "Jeep"
-//}
-//func (a *automobile) Model() string {
-//	return a.model
-//}
-//func (a *automobile) Length(num int, typ UnitType) Unit {
-//	return a.len
-//}
-//func (a *automobile) Width(num int, typ UnitType) Unit {
-//	return a.width
-//}
-//func (a *automobile) Height(num int, typ UnitType) Unit {
-//	return a.height
-//}
-//func NewAuto(brand, model string) *automobile {
-//	return &automobile{brand: brand, model: model}
-//}
