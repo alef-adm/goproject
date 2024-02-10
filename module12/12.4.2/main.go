4ff51fe3cfa1469a0e272f7c07a38101aed78505
alef-adm <ireg_2@mail.ru>
2024-02-10 14:32:49 +0300

    12.5.1

--- module12/12.4.2/main.go
+++ module12/12.4.2/main.go
@@ -11,38 +11,57 @@ func init() {
 }
 
 func main() {
-	ar := make([]int, 5)
+	ar := make([]int, 50)
 	for i := range ar {
 		ar[i] = rand.Intn(200) - 100 // ограничиваем случайное значение от [-100;100]
 	}
-
+	fmt.Println("до сортировки", ar)
 	selectionSort(ar)
 
-	fmt.Println(ar)
+	fmt.Println("после сортировки", ar)
 }
 
 func selectionSort(ar []int) {
-	for i := 0; i < len(ar); i++ {
-		start := i
-		finish := len(ar) - 1 - i
-		nMin := ar[start]
-		nMax := ar[finish]
-		fmt.Println(nMin, nMax)
-		var min_key, max_key int
-		//fmt.Println("start", start, "finish", finish)
-		for j := start; j == finish; j++ {
-			if ar[j] <= nMin {
-				nMin = ar[j]
-				min_key = j
+	for i := 0; i < len(ar)-i; i++ {
+		minK := i
+		maxK := len(ar) - 1 - i
+		var tmpmin, tmpmax int
+		for j := i; j < len(ar)-i; j++ {
+			fmt.Println("i=", i, "j=", j, "ar[j]=", ar[j], "ar[i]=", ar[i])
+			if ar[j] < ar[minK] {
+				minK = j
+				tmpmin = ar[minK]
+				fmt.Println("minK=", minK)
 			}
-			if ar[j] >= nMax {
-				nMax = ar[j]
-				max_key = j
+			if ar[j] > ar[maxK] {
+				maxK = j
+				tmpmax = ar[maxK]
+				fmt.Println("maxK=", maxK)
+
 			}
-			//fmt.Println(min_key, max_key)
 		}
-		ar[start], ar[min_key] = ar[min_key], ar[start]
-		ar[finish], ar[max_key] = ar[max_key], ar[finish]
+		if maxK == i && minK != len(ar)-1-i {
+			ar[len(ar)-1-i], ar[maxK] = ar[maxK], ar[len(ar)-1-i]
+			ar[i], ar[minK] = ar[minK], ar[i]
+		} else if minK == len(ar)-1-i && maxK != i {
+			ar[i], ar[minK] = ar[minK], ar[i]
+			ar[len(ar)-1-i], ar[maxK] = ar[maxK], ar[len(ar)-1-i]
+		} else if maxK == i && minK == len(ar)-1-i {
+			mTemp := ar[maxK]
+			ar[len(ar)-1-i], ar[maxK] = ar[maxK], ar[len(ar)-1-i]
+			ar[len(ar)-1-i] = mTemp
+		} else {
+			if minK != 0 && ar[minK] == tmpmin {
+				ar[i], ar[minK] = ar[minK], ar[i]
+
+			}
+			if maxK != 0 && ar[maxK] == tmpmax {
+				ar[len(ar)-1-i], ar[maxK] = ar[maxK], ar[len(ar)-1-i]
+
+			}
+		}
+		fmt.Println("minK=", minK, "maxK=", maxK)
+		fmt.Println("сортировка", i+1, "проход")
 		fmt.Println(ar)
 	}
 
